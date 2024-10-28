e7d9a7c3656ee16632170c8b9619db23f4d9fe36
nadeesha04 <161002770+nadeesha04@users.noreply.github.com>
2024-10-28 13:36:36 +0530

    Complete Our services part

--- foodi-client/src/pages/home/OurServices.jsx
+++ foodi-client/src/pages/home/OurServices.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
+const serviceLists = [
+  {
+    id: 1,
+    title: "CATERING",
+    des: "Delight your guests with our flavors and  presentation",
+    image: "/images/home/services/icon1.png",
+  },
+  {
+    id: 2,
+    title: "FAST DELIVERY",
+    des: "We deliver your order promptly to your door",
+    image: "/images/home/services/icon2.png",
+  },
+  {
+    id: 3,
+    title: "ONLINE ORDERING",
+    des: "Explore menu & order with ease using our Online Ordering ",
+    image: "/images/home/services/icon3.png",
+  },
+  {
+    id: 4,
+    title: "GIFT CARDS",
+    des: "Give the gift of exceptional dining with Foodi Gift Cards",
+    image: "/images/home/services/icon4.png",
+  },
+];
+
 const OurServices = () => {
   return (
     <div className="section-container my-6">
@@ -23,7 +50,18 @@ const OurServices = () => {
 
         {/* images */}
         <div className="md:w-1/2">
-          <img src="/images/home/testimonials/testimonials.png" alt="" />
+          <div className="grid sm:grid-cols-2 grid-cols-1 gap-8 items-center">
+            {serviceLists.map((service) => (
+              <div
+                key={service.id}
+                className="shadow-md rounded-sm py-5 px-4 text-center space-y-2 text-green cursor-pointer hover:border-indigo-600 transition-all duration-200 hover:border"
+              >
+                <img src={service.image} alt="" className="mx-auto" />
+                <h5 className="pt-3 font-semibold">{service.title}</h5>
+                <p className="text-[#90BD95]">{service.des}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
